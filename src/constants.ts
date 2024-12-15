b353a6442a9a0a098777fe973b1781b92694ffff
Keiyya Hassan <keiyya@Keiyyas-MacBook-Pro.local>
2024-12-15 20:25:38 +0300

    Disable Prev & Next

--- src/constants.ts
+++ src/constants.ts
@@ -1,4 +1,4 @@
 export const SITE_TITLE = "TechPeople Blog";
 export const SITE_DESCRIPTION = "Article, Stories & Tutorials For Tech People";
 export const HOMEPAGE_ARTICLE_LIMIT = 6;
-export const ARTICLE_PER_PAGE = 3;
+export const ARTICLE_PER_PAGE = 6;
