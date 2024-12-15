a75a224b677884fd6fdf060cf1a714474a548c64
Keiyya Hassan <keiyya@Keiyyas-MacBook-Pro.local>
2024-12-15 07:22:37 +0300

    Site constants

---
+++ src/constants.ts
@@ -0,0 +1,4 @@
+export const SITE_TITLE = "TechPeople Blog";
+export const SITE_DESCRIPTION = "Article, Stories & Tutorials For Tech People";
+export const HOMEPAGE_ARTICLE_LIMIT = 7;
+export const ARTICLE_PER_PAGE = 6;
